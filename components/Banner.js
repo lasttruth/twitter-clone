fd45a6139fa831b3ca92e4b43c6aedde402747c4
Robin Doirin <robindoirin@gmail.com>
2024-07-01 04:34:21 -0400

    sighin WiP

--- components/Banner.js
+++ components/Banner.js
@@ -1,6 +1,22 @@
-
 export default function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div className=" flex xl:space-x-[200px] justify-center items-center fixed w-full h-[80px] bg-[#1d9bf0] bottom-0">
+      <div className=" hidden xl:inline text-white">
+        <h1 className="text-2xl font-bold">Don't miss what's happening</h1>
+        <span className="text-[18px] font-normal">
+          Perple on Twitter are the first to know.
+        </span>
+      </div>
+      <div className=" space-x-3">
+        <button
+          className=" bg-transparent border border-white text-white
+        w-[100px] rounded-full h-[40px]"
+        >
+          Log In
+        </button>
+        <button className="bg-black border border-black  text-white
+         w-[100px] rounded-full h-[40px]">Sign Up</button>
+      </div>
+    </div>
+  );
 }
