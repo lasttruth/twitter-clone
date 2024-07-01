3e821d412b73c1c10f15ea2c558cb36fbcd4af54
Robin Doirin <robindoirin@gmail.com>
2024-07-01 04:00:01 -0400

    Trending component done

--- components/Trending.js
+++ components/Trending.js
@@ -1,8 +1,8 @@
-import { SearchIcon } from "@heroicons/react/outline";
-
+import { DotsHorizontalIcon, SearchIcon } from "@heroicons/react/outline";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
 export default function Trending() {
   return (
-    <div className=" lg:flex ">
+    <div className=" hidden lg:flex flex-col ml-7 mt-4 ">
       <div
         className="flex space-x-3 bg-white bg-opacity-10
         w-[300px] h-[44px] p-3 rounded-3xl mt-3"
@@ -17,7 +17,103 @@ export default function Trending() {
       </div>
 
       <div className="w-[300px] h-[500px] bg-white bg-opacity-10 rounded-3xl mt-3">
-
+        <h1 className="font-bold text-xl p-3">What's happening</h1>
+        <div className="p-3 relative">
+          <DotsHorizontalIcon className=" w-5 text-gray-600 absolute right-4" />
+          <p className="text-xs text-gray-500">Trending in US</p>
+          <h1 className="text-[15px] font-bold">China</h1>
+          <p className="text-xs text-gray-500">240k Tweets</p>
+        </div>
+        <div className="p-3 relative">
+          <DotsHorizontalIcon className=" w-5 text-gray-600 absolute right-4" />
+          <p className="text-xs text-gray-500">Trending in US</p>
+          <h1 className="text-[15px] font-bold">China</h1>
+          <p className="text-xs text-gray-500">240k Tweets</p>
+        </div>
+        <div className="p-3 relative">
+          <DotsHorizontalIcon className=" w-5 text-gray-600 absolute right-4" />
+          <p className="text-xs text-gray-500">Trending in US</p>
+          <h1 className="text-[15px] font-bold">China</h1>
+          <p className="text-xs text-gray-500">240k Tweets</p>
+        </div>
+        <div className="p-3 relative">
+          <DotsHorizontalIcon className=" w-5 text-gray-600 absolute right-4" />
+          <p className="text-xs text-gray-500">Trending in US</p>
+          <h1 className="text-[15px] font-bold">China</h1>
+          <p className="text-xs text-gray-500">240k Tweets</p>
+        </div>
+        <div className="p-3 relative">
+          <DotsHorizontalIcon className=" w-5 text-gray-600 absolute right-4" />
+          <p className="text-xs text-gray-500">Trending in US</p>
+          <h1 className="text-[15px] font-bold">China</h1>
+          <p className="text-xs text-gray-500">240k Tweets</p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[300px] bg-white bg-opacity-10 rounded-3xl mt-3">
+        <h1 className="font-bold text-xl p-3">Who to follow</h1>
+        <div className=" flex justify-between p-3">
+          <div className=" flex space-x-3">
+            <img
+              className="w-11 h-11 object-cover rounded-full"
+              src="/assets/bragg.png"
+            />
+            <div>
+              <div className=" flex space-x-1">
+                <h1 className=" font-bold">David Bragg</h1>
+                <BadgeCheckIcon className=" w-[18px] text-blue-400" />
+              </div>
+              <h1 className=" text-[12px] text-gray-500 mt-1">@davidbragg</h1>
+            </div>
+          </div>
+          <button
+            className=" bg-white text-black text-sm w-20 h-8
+          rounded-3xl font-bold "
+          >
+            Follow
+          </button>
+        </div>
+        <div className=" flex justify-between p-3">
+          <div className=" flex space-x-3">
+            <img
+              className="w-11 h-11 object-cover rounded-full"
+              src="/assets/bragg.png"
+            />
+            <div>
+              <div className=" flex space-x-1">
+                <h1 className=" font-bold">David Bragg</h1>
+                <BadgeCheckIcon className=" w-[18px] text-blue-400" />
+              </div>
+              <h1 className=" text-[12px] text-gray-500 mt-1">@davidbragg</h1>
+            </div>
+          </div>
+          <button
+            className=" bg-white text-black text-sm w-20 h-8
+          rounded-3xl font-bold "
+          >
+            Follow
+          </button>
+        </div>
+        <div className=" flex justify-between p-3">
+          <div className=" flex space-x-3">
+            <img
+              className="w-11 h-11 object-cover rounded-full"
+              src="/assets/bragg.png"
+            />
+            <div>
+              <div className=" flex space-x-1">
+                <h1 className=" font-bold">David Bragg</h1>
+                <BadgeCheckIcon className=" w-[18px] text-blue-400" />
+              </div>
+              <h1 className=" text-[12px] text-gray-500 mt-1">@davidbragg</h1>
+            </div>
+          </div>
+          <button
+            className=" bg-white text-black text-sm w-20 h-8
+          rounded-3xl font-bold "
+          >
+            Follow
+          </button>
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Banner from "@/components/Banner";
 import Postfeed from "@/components/Postfeed";
 import Sidebar from "@/components/Sidebar";
 import Trending from "@/components/Trending";
@@ -5,13 +6,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      className="bg-black min-h-screen text-[#e7e9ea]
+    <div>
+      <div
+        className="bg-black min-h-screen text-[#e7e9ea]
     max-w-[1400px] mx-auto flex"
-    >
-      {/* <Sidebar />
-      <Postfeed /> */}
-      <Trending />
+      >
+        <Sidebar />
+        <Postfeed />
+        <Trending />
+      </div>
+      <Banner />
     </div>
   );
 }
