84843be5f957314720cd688be449c0e7109f94dd
Robin Doirin <robindoirin@gmail.com>
2024-07-09 02:28:26 -0400

    made a tweet. populating firebase

--- components/TweetInput.js
+++ components/TweetInput.js
@@ -1,6 +1,31 @@
-import { CalendarIcon, ChartBarIcon, EmojiHappyIcon, LocationMarkerIcon, PhotographIcon } from "@heroicons/react/outline";
+import { db } from "@/firebase";
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+} from "@heroicons/react/outline";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function TweetInput() {
+  const user = useSelector((state) => state.user);
+  const [text, setText] = useState("");
+  async function sendTweet() {
+    const docRef = await addDoc(collection(db, "posts"), {
+      username: user.username,
+      name: user.name,
+      photoUrl: user.photoUrl,
+      uid: user.uid,
+      timestamp: serverTimestamp(),
+      likes: [],
+      tweet: text,
+    });
+
+    setText("");
+  }
   return (
     <div className="flex space-x-3 p-3 border-b border-gray-700">
       <img
@@ -12,6 +37,8 @@ export default function TweetInput() {
           placeholder="What's on your mind?"
           className="bg-transparent resize-none outline-none w-full
         min-h-[50px] text-lg"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
         />
 
         <div className="flex justify-between border-t border-gray-700 pt-4">
@@ -34,8 +61,12 @@ export default function TweetInput() {
             </div>
           </div>
           <button
-          className="bg-[#1d9bf0] rounded-full px-4 py-1.5"
-          >Tweet</button>
+            onClick={sendTweet}
+            disabled={!text}
+            className="bg-[#1d9bf0] rounded-full px-4 py-1.5 disabled:opacity-50"
+          >
+            Tweet
+          </button>
         </div>
       </div>
     </div>
